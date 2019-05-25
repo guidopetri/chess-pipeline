4173cb195ec42eea7f964e7450e33627afc2574a
Charles Black <18634426+charlesoblack@users.noreply.github.com>
2019-05-25 19:10:21 +0200

    initial commit

---
+++ chess_pipeline.py
@@ -0,0 +1,136 @@
+#! /usr/bin/env python3
+
+from luigi.parameter import Parameter, ListParameter, IntParameter
+from luigi.util import requires, inherits
+from luigi.format import UTF8, Nop
+from luigi import Task
+from luigi.mock import MockTarget
+from postgres_templates import CopyWrapper, HashableDict, TransactionFactTable
+from datetime import datetime, timedelta
+
+
+class FetchLichessApi(Task):
+
+    player = Parameter(default='thibault')
+    perfType = Parameter(default='blitz')
+    since = IntParameter(default=None)
+
+    def output(self):
+        return MockTarget('LichessGames %s' % self.player, format=Nop)
+
+    def run(self):
+        import lichess.api
+        from lichess.format import PYCHESS
+        from pandas import DataFrame
+
+        if self.since is None:
+            two_days_ago = datetime.today() - timedelta(days=2)
+            unix_time = two_days_ago.timestamp()
+            self.since = int(1000 * unix_time)
+
+        games = lichess.api.user_games(self.player,
+                                       since=self.since,
+                                       perfType=self.perfType,
+                                       format=PYCHESS)
+
+        header_infos = []
+        for game in games:
+            header_infos.append({x: y for x, y in game.headers.items()})
+
+        df = DataFrame(header_infos)
+
+        with self.output().open('w') as f:
+            df.to_pickle(f, compression=None)
+
+
+@requires(FetchLichessApi)
+class CleanChessDF(Task):
+
+    columns = ListParameter()
+
+    def output(self):
+        return MockTarget('CleanChessDF', format=UTF8)
+
+    def run(self):
+        from pandas import read_pickle, to_datetime
+
+        with self.input().open('r') as f:
+            df = read_pickle(f, compression=None)
+
+        df['Date'] = to_datetime(df['Date'])
+        df['UTCDate'] = to_datetime(df['UTCDate'])
+
+        rating_columns = ['BlackElo',
+                          'BlackRatingDiff',
+                          'WhiteElo',
+                          'WhiteRatingDiff',
+                          ]
+
+        for column in rating_columns:
+            df[column] = df[column].astype(int)
+
+        df.rename(columns={'Black':           'black',
+                           'BlackElo':        'black_elo',
+                           'BlackRatingDiff': 'black_rating_diff',
+                           'Date':            'date_played',
+                           'ECO':             'opening_played',
+                           'Event':           'event_type',
+                           'Result':          'result',
+                           'Round':           'round',
+                           'Site':            'game_link',
+                           'Termination':     'termination',
+                           'TimeControl':     'time_control',
+                           'UTCDate':         'utc_date_played',
+                           'UTCTime':         'time_played',
+                           'Variant':         'chess_variant',
+                           'White':           'white',
+                           'WhiteElo':        'white_elo',
+                           'WhiteRatingDiff': 'white_rating_diff',
+                           },
+                  inplace=True)
+
+        df = df[list(self.columns)]
+
+        with self.output().open('w') as db:
+            db.write(df.to_csv(sep='\t',
+                               index=False,
+                               header=False,
+                               na_rep='\\N',
+                               line_terminator='\n',
+                               ))
+
+
+@requires(CleanChessDF)
+class ChessGames(TransactionFactTable):
+
+    pass
+
+
+@inherits(FetchLichessApi)
+class CopyGames(CopyWrapper):
+
+    jobs = [{'table_type': ChessGames,
+             'fn':         CleanChessDF,
+             'table':      'chess_games',
+             'columns':    ['black',
+                            'black_elo',
+                            'black_rating_diff',
+                            'date_played',
+                            'opening_played',
+                            'event_type',
+                            'result',
+                            'round',
+                            'game_link',
+                            'termination',
+                            'time_control',
+                            'utc_date_played',
+                            'time_played',
+                            'chess_variant',
+                            'white',
+                            'white_elo',
+                            'white_rating_diff',
+                            ],
+             'id_col':     'game_link',
+             'date_cols':  ['date_played', 'utc_date_played'],
+             'merge_cols': HashableDict()},
+            ]
---
+++ postgres_templates.py
@@ -0,0 +1,113 @@
+#! /usr/bin/env python3
+
+from collections import OrderedDict
+from luigi.contrib import postgres
+from luigi.parameter import Parameter, ListParameter, DictParameter
+from luigi.parameter import IntParameter, TaskParameter, ParameterVisibility
+from luigi import WrapperTask
+
+
+class HashableDict(OrderedDict):
+    def __hash__(self):
+        return hash(frozenset(self))
+
+
+class TransactionFactTable(postgres.CopyToTable):
+    """
+    Copy a pandas DataFrame in a transaction fact table fashion to PostGreSQL.
+
+    :param user: The username to use to authenticate with PostGreSQL.
+    :param password: The password to use to authenticate with PostGreSQL.
+    :param host: The hostname where the PostGreSQL server is located.
+    :param port: The port that PostGreSQL is listening to.
+    :param database: The database name to write to.
+    :param table: The table to write to.
+    :param columns: The columns, in order they show up in the PostGreSQL table.
+    :param fn: The Task that provides the data to load.
+    :param id_col: The column to be used to identify whether a row is already
+                   in the table.
+    :param merge_cols: The columns representing dimension tables. In a
+                       dictionary format, with the key being the `left` to
+                       merge with, and the value being a tuple of
+                       (`table`, `right`).
+    """
+
+    user = Parameter(visibility=ParameterVisibility.PRIVATE)
+    password = Parameter(visibility=ParameterVisibility.PRIVATE)
+    host = Parameter(visibility=ParameterVisibility.PRIVATE)
+    port = IntParameter(visibility=ParameterVisibility.PRIVATE)
+    database = Parameter(visibility=ParameterVisibility.PRIVATE)
+    table = Parameter()
+    fn = TaskParameter()
+    columns = ListParameter()
+    id_col = Parameter()
+    merge_cols = DictParameter()
+
+    column_separator = '\t'
+
+    def requires(self):
+        return self.clone(self.fn)
+
+    def rows(self):
+        from pandas import read_pickle, DataFrame
+
+        connection = self.output().connect()
+        cursor = connection.cursor()
+
+        sql = """SELECT %s FROM %s;""" % (self.id_col, self.table)
+
+        cursor.execute(sql)
+        results = cursor.fetchall()
+
+        current_df = DataFrame(results, columns=['id_col'])
+
+        with self.input().open('r') as f:
+            df = read_pickle(f, compression=None)
+
+        df = df[~df[self.id_col].isin(current_df['id_col'].values.tolist())]
+        df = df[list(self.columns)]
+
+        for index, line in df.iterrows():  # returns (index,Series) tuple
+            yield line.values.tolist()
+
+
+class CopyWrapper(WrapperTask):
+    """
+    Dynamically require each Task in the `jobs` list.
+
+    In order to write to multiple tables, we need to pass in a different
+    `table` parameter each time. Using the `jobs` list, we can pass in a `dict`
+    that has the appropriate parameters for each requirement.
+
+    :param password: The PostGreSQL password.
+    :param jobs: List of dictionaries. Each dictionary specifies one Task to
+                 run and copy to a PostGreSQL table. Necessary keys are:
+
+                 - `table`: The PostGreSQL table.
+                 - `fn`: The Task to run in order to get the data.
+                 - `columns: The table's columns, in the order they show up in
+                             the PostGreSQL table.
+                 - `id_col`: The column to be used to identify whether a row is
+                             already present.
+                 - `date_cols`: The columns that have to be converted to
+                                datetime before copying.
+                 - `merge_cols`: The columns that should represent dimension
+                                 tables. This should be in the form of a
+                                 `HashableDict()`, with the `left` column name
+                                 as key, and a tuple of (`table`, `right`) as
+                                 value.
+    """
+
+    password = Parameter(visibility=ParameterVisibility.PRIVATE)
+
+    jobs = []
+
+    def requires(self):
+        for job in self.jobs:
+            self.table = job['table']
+            self.fn = job['fn']
+            self.columns = job['columns']
+            self.id_col = job['id_col']
+            self.date_cols = job['date_cols']
+            self.merge_cols = job['merge_cols']
+            yield self.clone(job['table_type'])

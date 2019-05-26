5fdfecf7d4c8decec901da3d5e1a4886e8a6483d
Charles Black <18634426+charlesoblack@users.noreply.github.com>
2019-05-26 10:39:24 +0200

    add example sql, readme and requirements

---
+++ chess_games.sql
@@ -0,0 +1,20 @@
+create table chess_games(
+    id                serial  primary key,
+    black             text    not null,
+    black_elo         integer not null,
+    black_rating_diff integer not null,
+    date_played       date    not null,
+    opening_played    text    not null,
+    event_type        text    not null,
+    result            text    not null,
+    round             text,
+    game_link         text    not null,
+    termination       text    not null,
+    time_control      text    not null,
+    utc_date_played   date    not null,
+    time_played       time    not null,
+    chess_variant     text    not null,
+    white             text    not null,
+    white_elo         integer not null,
+    white_rating_diff integer not null
+);

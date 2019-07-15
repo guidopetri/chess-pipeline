2840c9b0d66103e95e2a2b488784d9b2ad07c2ce
Guido Petri <18634426+guidopetri@users.noreply.github.com>
2019-07-15 21:27:28 +0200

    first version of setup SQL file

---
+++ setup_postgresql.sql
@@ -0,0 +1,46 @@
+-- this file will set up the entire structure for chess_db
+
+-- overview:
+-- create the main database and the task history database
+-- create the users for luigi, read access and task history
+-- grant privileges to respective users
+
+-- create chess_db
+create database chess_db;
+\c chess_db
+
+-- create task_history_db
+create database task_history_db;
+
+-- make sure you have this file in the folder you're in, or change the location
+\i chess_games.sql
+
+-- create the user that luigi will be using
+create user luigi_user;
+\password luigi_user
+
+--grant luigi user privileges on database
+
+
+-- the task history user
+create user task_history_user;
+\password task_history_user
+
+-- grant task history user privileges on task history db
+
+
+-- a read-only user
+create user read_user;
+\password read_user
+
+-- read-only role
+create role read_access;
+
+-- grant access to existing tables
+grant usage on schema public to read_access;
+grant select on all tables in schema public to read_access;
+
+-- grant access to future tables
+alter default privileges in schema public grant select on tables to read_access;
+
+grant read_access TO read_user;

1d2dce7c808429d7e29ad6de0146ccc6712b8d6c
Charles Black <18634426+charlesoblack@users.noreply.github.com>
2019-07-19 00:07:40 +0200

    get games from lichess API directly in PGN format

--- chess_pipeline.py
+++ chess_pipeline.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 
 from luigi.parameter import Parameter, ListParameter, IntParameter
-from luigi.parameter import DateParameter
+from luigi.parameter import DateParameter, ParameterVisibility
 from luigi.util import requires, inherits
 from luigi.format import Nop
 from luigi import Task, LocalTarget
@@ -9,47 +9,80 @@ from postgres_templates import CopyWrapper, HashableDict, TransactionFactTable
 from datetime import datetime, timedelta
 
 
-class FetchLichessApi(Task):
+class FetchLichessApiPGN(Task):
 
     date = DateParameter(default=datetime.today())
     player = Parameter(default='thibault')
     perfType = Parameter(default='blitz')
     since = IntParameter(default=None)
+    lichess_token = Parameter(visibility=ParameterVisibility.PRIVATE,
+                              significant=False)
 
     def output(self):
         import os
 
-        file_location = '~/Temp/luigi/raw-games-%s.pckl' % self.player
+        file_location = '~/Temp/luigi/raw-games-{}.pckl'.format(self.player)
         return LocalTarget(os.path.expanduser(file_location), format=Nop)
 
     def run(self):
-        import lichess.api
-        from lichess.format import PYCHESS
+        import requests
         from pandas import DataFrame
+        from time import sleep
 
         self.output().makedirs()
 
         if self.since is None:
-            two_days_ago = datetime.today() - timedelta(days=2)
+            two_days_ago = datetime.today() - timedelta(days=7)
             unix_time = two_days_ago.timestamp()
             self.since = int(1000 * unix_time)
 
-        games = lichess.api.user_games(self.player,
-                                       since=self.since,
-                                       perfType=self.perfType,
-                                       format=PYCHESS)
+        url = 'https://lichess.org/api/games/user/{}'.format(self.player)
+        headers = {'Accept': 'application/x-chess-pgn',
+                   'Authorization': 'Bearer {}'.format(self.lichess_token)}
+        params = {'perfType': self.perfType,
+                  'since': self.since,
+                  'opening': True,
+                  'moves': False,  # for now
+                  }
 
-        header_infos = []
-        for game in games:
-            header_infos.append({x: y for x, y in game.headers.items()})
+        params = {k: v for k, v in params.items() if v is not None}
+
+        while True:
+            resp = requests.get(url,
+                                headers=headers,
+                                params=params,
+                                stream=True)
+
+            if resp.status_code == 429:
+                sleep(60)
+                continue
+            break
+
+        header_infos = [{x: y for x, y in pgn.headers.items()}
+                        for pgn in self.stream_pgns(resp)]
 
         df = DataFrame(header_infos)
 
         with self.output().open('w') as f:
             df.to_pickle(f, compression=None)
 
+    def stream_pgns(self, resp):
+        from chess.pgn import read_game
+        from io import StringIO
 
-@requires(FetchLichessApi)
+        buffer = []
+        for line in resp.iter_lines():
+            buffer.append(line.decode('utf-8'))
+            # pgns are separated by two newlines
+            if buffer[-2:] == ['', '']:
+                yield read_game(StringIO('\n'.join(buffer)))
+                buffer = []
+        # after the last pgn, there are 3 newlines
+        if len(buffer) > 3:
+            yield read_game(StringIO('\n'.join(buffer)))
+
+
+@requires(FetchLichessApiPGN)
 class CleanChessDF(Task):
 
     columns = ListParameter()
@@ -57,7 +90,7 @@ class CleanChessDF(Task):
     def output(self):
         import os
 
-        file_location = '~/Temp/luigi/cleaned-games-%s.pckl' % self.player
+        file_location = '~/Temp/luigi/clean-games-{}.pckl'.format(self.player)
         return LocalTarget(os.path.expanduser(file_location), format=Nop)
 
     def run(self):
@@ -197,7 +230,7 @@ class ChessGames(TransactionFactTable):
     pass
 
 
-@inherits(FetchLichessApi, ChessGames)
+@inherits(FetchLichessApiPGN, ChessGames)
 class CopyGames(CopyWrapper):
 
     jobs = [{'table_type': ChessGames,

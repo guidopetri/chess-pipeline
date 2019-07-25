e420c9a99fda3ceb1a9f520fdec11b5affd038d2
Charles Black <18634426+charlesoblack@users.noreply.github.com>
2019-07-26 00:22:39 +0200

    remove castling table

--- table-sql/castling.sql
+++
@@ -1,7 +0,0 @@
-create table castling(
-    id            serial unique not null,
-    game          text          not null,
-    side          text          not null,
-    castling_side text          not null,
-    primary key (game, side)
-);

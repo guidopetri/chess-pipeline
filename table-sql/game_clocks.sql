02955881b1ad5e6ed155b2d17a0564629b962eac
Guido Petri <18634426+guidopetri@users.noreply.github.com>
2019-09-01 21:57:33 -0400

    clocks table sql

---
+++ table-sql/game_clocks.sql
@@ -0,0 +1,7 @@
+create table game_clocks(
+id            serial   primary key,
+game_link     text     not null,
+-- we don't need 4 bytes so we may as well save space and use smallint
+half_move     smallint not null,
+clock         text     not null
+);

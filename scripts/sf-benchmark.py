1816ccae7541b7369008550249e9ff973ceb9728
Guido Petri <18634426+guidopetri@users.noreply.github.com>
2020-07-26 13:39:19 -0500

    add benchmark code

---
+++ scripts/sf-benchmark.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python3
+
+import timeit
+import stockfish
+
+
+def run_benchmark(stockfish_location):
+    times = []
+
+    fen = 'rnb1k1nr/pp1p1ppp/1q2p3/2b5/4P3/5N2/PPP1QPPP/RNB1KB1R b KQkq - 2 5'
+
+    for n in range(10, 26):
+        sf = stockfish.Stockfish(stockfish_location, depth=n)  # noqa
+
+        statement = f'sf.set_fen_position({fen}); sf.get_best_move()'
+        time_taken = timeit.timeit(statement, number=100)
+
+        times.append(time_taken)
+
+    print(times)
+    return
+
+
+if __name__ == '__main__':
+    import sys
+
+    if len(sys) < 2:
+        raise ValueError('Must provide stockfish executable location')
+    stockfish_location = sys.argv[1]
+
+    run_benchmark(stockfish_location)

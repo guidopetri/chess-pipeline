607dda59f4d31c0b5895f66a51d0bbc8d6eaa963
Guido Petri <18634426+guidopetri@users.noreply.github.com>
2020-08-02 14:07:20 -0500

    add sql files for win probabilities table

---
+++ scripts/assorted-sql/copy_win_probabilities.sql
@@ -0,0 +1,1 @@
+\copy eco_codes(eco_code, opening_name, opening_notes) from '../res/content/win_probabilities.csv' with csv delimiter ',';
---
+++ src/table-sql/win_probabilities.sql
@@ -0,0 +1,6 @@
+create table win_probabilities(
+id serial primary key,
+eval real not null,
+probability_lr real not null,
+probability_bayes real not null
+);

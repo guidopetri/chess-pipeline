eca25e17924420748692988f0f27c78a5ca6bb80
Guido Petri <18634426+guidopetri@users.noreply.github.com>
2020-08-04 12:29:45 -0500

    ignore cloud eval for now

--- src/pipeline_import/api.py
+++ src/pipeline_import/api.py
@@ -2,31 +2,7 @@
 
 
 def get_cloud_eval(fen, variation_count=1, variant='standard'):
-    import requests
-    import time
 
-    cloud_eval_url = 'https://lichess.org/api/cloud-eval'
-    params = {'fen': fen,
-              'multiPv': variation_count,
-              'variant': variant,
-              }
+    # ignore cloud eval for now because of API issues
 
-    while True:
-        r = requests.get(cloud_eval_url, params=params)
-
-        if r.status_code == 429:
-            time.sleep(60)
-        elif r.status_code == 200:
-            break
-
-    data = r.json()
-
-    if 'error' in data:
-        return {}
-
-    reformed_data = {'depth': data['depth'],
-                     'evaluations': [pv['cp']
-                                     for pv in data['pvs']],
-                     }
-
-    return reformed_data
+    return {}

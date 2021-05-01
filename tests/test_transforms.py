bf0ac4fb3bc2e01754f31bb5944835feee8b5678
Guido Petri <18634426+guidopetri@users.noreply.github.com>
2021-04-30 22:27:52 -0400

    add tests backbone

--- tests/test_transforms.py
+++ tests/test_transforms.py
@@ -4,6 +4,22 @@ from pipeline_import import transforms
 import pandas as pd
 
 
+def test_parse_headers():
+    assert False
+
+
+def test_fix_provisional_columns():
+    assert False
+
+
+def test_get_sf_evaluation():
+    assert False
+
+
+def test_convert_clock_to_seconds():
+    assert False
+
+
 def test_get_clean_fens():
     fen = 'r3rnk1/ppq2ppn/2pb4/3pN1p1/3P1P1B/2PB4/PPQ3PP/R3R1K1 w - - 0 19'
     clean_fen = 'r3rnk1/ppq2ppn/2pb4/3pN1p1/3P1P1B/2PB4/PPQ3PP/R3R1K1 w - - 0'
@@ -11,3 +27,19 @@ def test_get_clean_fens():
     fen = pd.Series([fen])
     clean_fen = pd.Series([clean_fen])
     assert (transforms.get_clean_fens(fen) == clean_fen).all()
+
+
+def test_transform_game_data():
+    assert False
+
+
+def test_get_color_stats():
+    assert False
+
+
+def test_get_elo_by_weekday():
+    assert False
+
+
+def test_get_weekly_data():
+    assert False
---
+++ tests/test_visitors.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python3
+
+from pipeline_import import visitors  # noqa
+
+
+def test_evals_visitor():
+    assert False
+
+
+def test_clocks_visitor():
+    assert False
+
+
+def test_queen_exchange_visitor():
+    assert False
+
+
+def test_castling_visitor():
+    assert False
+
+
+def test_positions_visitor():
+    assert False
+
+
+def test_promotions_visitor():
+    assert False
+
+
+def test_materials_visitor():
+    assert False

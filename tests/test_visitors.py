59ad298d54874c5d9888e36f633eed4fc30493b2
Guido Petri <18634426+guidopetri@users.noreply.github.com>
2021-04-30 21:58:03 -0400

    add test for get_clean_fens

---
+++ tests/test_visitors.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python3
+
+import pipeline_import.transforms as transforms
+import pandas as pd
+
+
+def test_get_clean_fens():
+    fen = 'r3rnk1/ppq2ppn/2pb4/3pN1p1/3P1P1B/2PB4/PPQ3PP/R3R1K1 w - - 0 19'
+    clean_fen = 'r3rnk1/ppq2ppn/2pb4/3pN1p1/3P1P1B/2PB4/PPQ3PP/R3R1K1 w - - 0'
+
+    fen = pd.Series([fen])
+    clean_fen = pd.Series([clean_fen])
+    assert (transforms.get_clean_fens(fen) == clean_fen).all()

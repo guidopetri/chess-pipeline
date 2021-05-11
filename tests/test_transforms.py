d8e396805cd61f63c5f577636a8e3b0adb141cdd
Guido Petri <18634426+guidopetri@users.noreply.github.com>
2021-05-10 23:08:00 -0400

    test color stats

--- tests/test_transforms.py
+++ tests/test_transforms.py
@@ -553,7 +553,35 @@ def test_transform_game_data():
 
 
 def test_get_color_stats():
-    assert False
+
+    data = pd.DataFrame([[13, 'blitz', 'white', 'Win'],
+                         [14, 'blitz', 'white', 'Loss'],
+                         [15, 'blitz', 'white', 'Draw'],
+                         [16, 'blitz', 'black', 'Win'],
+                         [17, 'blitz', 'black', 'Win'],
+                         [18, 'bullet', 'black', 'Win'],
+                         ],
+                        columns=['id',
+                                 'time_control_category',
+                                 'player_color',
+                                 'player_result',
+                                 ])
+
+    parsed = transforms.get_color_stats(data)
+
+    multiindex = pd.MultiIndex.from_arrays([['blitz', 'blitz', 'bullet'],
+                                            ['white', 'black', 'black']],
+                                           names=('time_control_category',
+                                                  'player_color'))
+
+    true = pd.DataFrame([[1 / 3, 1 / 3, 1 / 3],
+                         [1, 0, 0],
+                         [1, 0, 0]],
+                        columns=['Win', 'Draw', 'Loss'],
+                        index=multiindex,
+                        )
+
+    pd.testing.assert_frame_equal(parsed, true, check_like=True)
 
 
 def test_get_elo_by_weekday():

0a7e0d86a8bb23cd31f77887236d11860d3ef73e
Guido Petri <18634426+guidopetri@users.noreply.github.com>
2021-11-06 16:20:44 -0400

    fix text in winratiobycolor

--- src/newsletter.py
+++ src/newsletter.py
@@ -64,15 +64,47 @@ class WinRatioByColor(Task):
                               fig_loc=fig_loc,
                               filename=filename)
 
-        text = ('You had a {:.2f}% win rate with {} in {}'
-                ' and a {:.2f}% win rate with {}. <br>'
-                '<img alt=\'Win rate by color played\' src='
-                '\'cid:win-by-color\'><br>'
-                .format(100 * color_stats.iloc[0][0],
-                        *color_stats.iloc[0].name[::-1],
-                        100 * color_stats.iloc[1][0],
-                        color_stats.iloc[1].name[1],
-                        ))
+        color_stats.reset_index(inplace=True, drop=False)
+
+        category = color_stats['time_control_category'].head(1).item()
+        results = color_stats[color_stats['time_control_category'] == category]
+        first_results = results.head(1)
+        first_color = first_results['player_color'].item()
+        first_win_rate = first_results['Win'].item() * 100
+        first_win_rate_str = (f'You had a {first_win_rate:.2f}% win rate'
+                              f' with {first_color} in {category}')
+
+        if color_stats.shape[0] > 1:
+            if results.shape[0] > 1:
+                results = results.tail(1)
+                second_category_substr = ''
+            else:
+                # second row should be a different category
+                results = color_stats.iloc[[1]]
+
+                # results should be non-empty no matter what
+                # since we have 2 rows at least, and either we have the same
+                # category (we don't hit this line) or we have different
+                # categories (this line is valid)
+                second_category = results['time_control_category'].item()
+                second_category_substr = ' in ' + second_category
+
+            second_color = results['player_color'].item()
+            second_win_rate = results['Win'].item() * 100
+
+            second_win_rate_str = (f' and a {second_win_rate:.2f}% win rate '
+                                   f'with {second_color}'
+                                   f'{second_category_substr}')
+
+        else:
+            second_win_rate_str = ''
+
+        win_rate_string = first_win_rate_str + second_win_rate_str + '.'
+
+        text = (win_rate_string
+                + ' <br> <img alt=\'Win rate by color '
+                + 'played\' src=\'cid:win-by-color\'><br>'
+                )
 
         with self.output().open('w') as f:
             pickle.dump(text, f, protocol=-1)

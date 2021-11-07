41e21304be7ee09b2765ad2634a2b89c7d8bd69d
Guido Petri <18634426+guidopetri@users.noreply.github.com>
2021-11-07 12:33:42 -0500

    update win probabilities table sql

--- src/table-sql/win_probabilities.sql
+++ src/table-sql/win_probabilities.sql
@@ -1,5 +1,7 @@
 create table win_probabilities(
-id                  serial primary key,
-eval                real not null,
-probability_lr      real not null,
+id                      serial   primary key,
+game_link               text     not null,
+half_move               smallint not null,
+win_probability_white   real     not null,
+win_prob_model_version  text     not null
 );

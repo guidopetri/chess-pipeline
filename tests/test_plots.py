06862ce4f93f2e19d0b71699cd0204951169f27b
Guido Petri <18634426+guidopetri@users.noreply.github.com>
2024-05-04 20:10:19 -0500

    add more test coverage

--- tests/test_plots.py
+++ tests/test_plots.py
@@ -1,11 +1,20 @@
 #! /usr/bin/env python3
 
+import contextlib
 import hashlib
 import os
+import pickle
+from io import BytesIO
 
 import pandas as pd
 from pipeline_import import plots
-from utils.newsletter import get_color_stats_text
+from utils.newsletter import (
+    create_newsletter,
+    generate_elo_by_weekday_text,
+    generate_win_ratio_by_color_text,
+    get_color_stats_text,
+    send_newsletter,
+)
 
 
 def test_color_stats_plot():
@@ -161,3 +170,85 @@ def test_color_stats_text_multiple_categories():
                          ' a 100.00% win rate with black in bullet.')
 
     assert win_rate_str == true_win_rate_str
+
+
+def test_generate_elo_by_weekday_text_empty():
+    input_df = pd.DataFrame()
+    text = generate_elo_by_weekday_text(input_df, '', '')
+
+    assert text == '\n'
+
+
+def test_generate_elo_by_weekday_text_generic(mocker, snapshot):
+    # TODO: split this function off so it doesn't have to be patched
+    mocker.patch('utils.newsletter.make_elo_by_weekday_plot')
+
+    elo_df = pd.DataFrame([[100, 200], [200, 300]], columns=['min', 'max'])
+
+    mocker.patch('utils.newsletter.get_elo_by_weekday', return_value=elo_df)
+
+    text = generate_elo_by_weekday_text(elo_df, 'bullet', 'thibault')
+
+    assert text == snapshot
+
+
+def test_generate_win_ratio_by_color_text_empty(snapshot):
+    input_df = pd.DataFrame()
+    text = generate_win_ratio_by_color_text(input_df, '')
+
+    assert text == snapshot
+
+
+def test_generate_win_ratio_by_color_text_generic(mocker, snapshot):
+    mocker.patch('utils.newsletter.get_color_stats')
+    mocker.patch('utils.newsletter.get_color_stats_text', return_value='foo')
+    # TODO: split this function off so it doesn't have to be patched
+    mocker.patch('utils.newsletter.make_color_stats_plot')
+    text = generate_win_ratio_by_color_text(pd.DataFrame([0]), '')
+
+    assert text == snapshot
+
+
+def test_send_newsletter(tmp_path, mocker):
+    mocker.patch('utils.newsletter.sendgrid')
+    mock_send = mocker.patch('utils.newsletter.SendGridAPIClient.send')
+    mock_send.return_value.status_code = 202
+
+    mocker.patch('os.path.expanduser', return_value=tmp_path / 'files')
+    os.makedirs(tmp_path / 'files' / 'foobar')
+
+    with open(tmp_path / 'files' / 'foo.bar', 'w') as f:
+        f.write('dummy-input')
+
+    text = 'foo'
+
+    assert send_newsletter(text)
+    mock_send.assert_called_once_with(text)
+    assert not list((tmp_path / 'files').glob('*'))
+
+
+def test_create_newsletter(tmp_path, mocker, snapshot):
+    mock_cfg = mocker.patch('utils.newsletter.newsletter_cfg')
+    mock_cfg.sender = 'foo@bar.com'
+
+    mocker.patch('os.path.expanduser', return_value=tmp_path / 'images')
+    os.mkdir(tmp_path / 'images')
+
+    with open(tmp_path / 'images' / 'thibault.png', 'w') as f:
+        f.write('dummy-input')
+
+    class FakeLuigiInput:
+        def __init__(self, value):
+            self.value = value
+
+        @contextlib.contextmanager
+        def open(self, *args):
+            return (x for x in [self.value])
+
+    inputs = [FakeLuigiInput(BytesIO(pickle.dumps('foo'))),
+              FakeLuigiInput(BytesIO(pickle.dumps('bar'))),
+              ]
+
+    newsletter = create_newsletter(inputs, 'thibault', 'bar@foo.com')
+
+    assert newsletter == snapshot
